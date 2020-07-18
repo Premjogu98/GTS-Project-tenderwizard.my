c8c3960f6797fff273d9a56a9e252993f1b27416
premjogu007 <47113551+premjogu007@users.noreply.github.com>
2020-07-18 09:58:37 +0530

    first commit

---
+++ Navigation_page.py
@@ -0,0 +1,96 @@
+from selenium import webdriver
+import time
+import html
+import sys, os
+from datetime import datetime,timedelta
+import Global_var
+import wx
+import string
+import re
+import string
+from Scraping_things import scrap_data
+app = wx.App()
+
+def chormedriver():
+    browser = webdriver.Chrome(executable_path=str(f"C:\\chromedriver.exe"))
+    browser.get('https://www.tenderwizard.my/')
+    browser.maximize_window()
+    time.sleep(8)
+    get_links(browser)
+
+def get_links(browser):
+    links_list = []
+    for links in browser.find_elements_by_xpath('//*[@class="hm_pg_list"]/li/a'):
+        links_list.append(links.get_attribute('href'))
+
+    for link in links_list:
+        error = True
+        while error == True:
+            try:
+                browser.get(link)
+                time.sleep(5)
+                try:
+                    for close_tooltip in browser.find_elements_by_xpath("//*[@class='fa fa-times cst-icon-2']"):
+                        close_tooltip.click()
+                        break
+                except:pass
+                pos = [] #list to store positions for each 'char' in 'string'
+                for n in range(len(link)):
+                    if link[n] == '/':
+                        pos.append(n)
+                purchaser_name = link[pos[-1]:]
+                purchaser_name = purchaser_name.replace('/','')
+                custom_tender_link = f'https://www.tenderwizard.my/ROOTAPP/tenderfreeview.jsp?company={purchaser_name}'
+                browser.get(custom_tender_link)
+                time.sleep(3)
+                tender_links_list = []
+                inthere = True
+                page_no = 1
+                for next_page in browser.find_elements_by_xpath("//*[@class='pagination']/tbody/tr/td"):
+                    for get_links in browser.find_elements_by_xpath("//*[@class='tblsummary']/tbody/tr/td[5]/a"):
+                        get_links = get_links.get_attribute('href')
+                        get_links =  get_links.partition("('")[2].partition("');")[0]
+                        main_tender_link = f"https://www.tenderwizard.my/ROOTAPP/{get_links}"
+                        tender_links_list.append(main_tender_link)
+                        inthere = False
+                    if page_no != 1:
+                        browser.get(f'https://www.tenderwizard.my/ROOTAPP/NewTenderFreeView.jsp?&ymns={str(page_no)}&ymnsl=1&iPageStart=2&iStart=1&col1=&col2=&col3=&col4=&col5=&col6=&col7=&')
+                        time.sleep(2)
+                    page_no += 1
+                if inthere == True:
+                        # wx.MessageBox(' Nothing there to collect link ','tenderwizard.my', wx.OK | wx.ICON_INFORMATION)
+                        time.sleep(2)
+                else:
+                    for tender_link in tender_links_list:
+                        browser.get(tender_link)
+                        time.sleep(3)
+                        get_htmlsource = ''
+                        for purchaser_tbl in browser.find_elements_by_xpath("//*[@class='info']"):
+                            purchaser_tbl = purchaser_tbl.get_attribute('outerHTML')
+                            purchaser_tbl = re.sub(' +', ' ', str(purchaser_tbl))
+                            purchaser_tbl = html.unescape(str(purchaser_tbl))
+                            get_htmlsource += purchaser_tbl.replace('-\t','').replace('-\n','').replace('\t','').replace('\n','')
+                            break
+                        for tenderDetail in browser.find_elements_by_xpath('//*[@id="tenderDetail"]'):
+                            tenderDetail = tenderDetail.get_attribute('outerHTML')
+                            tenderDetail = re.sub(' +', ' ', str(tenderDetail))
+                            tenderDetail = html.unescape(str(tenderDetail))
+                            get_htmlsource +=tenderDetail.replace('-\t','').replace('-\n','').replace('\t','').replace('\n','')
+                            break
+                        if get_htmlsource == '':
+                                wx.MessageBox(' get_htmlsource Blank ','tenderwizard.my', wx.OK | wx.ICON_INFORMATION)
+                        else:
+                            scrap_data(browser,get_htmlsource,tender_link)
+                            Global_var.Total += 1
+                            print(f'Total: {str(Global_var.Total)} Deadline Not given: {Global_var.deadline_Not_given} duplicate: {Global_var.duplicate} inserted: {Global_var.inserted} expired: {Global_var.expired} QC Tenders: {Global_var.QC_Tenders}')
+                error = False
+            except Exception as e:
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print("Error ON : ", sys._getframe().f_code.co_name + "--> " + str(e), "\n", exc_type, "\n", fname, "\n",exc_tb.tb_lineno)
+                error = True
+                time.sleep(5)
+    wx.MessageBox(f'Total: {str(Global_var.Total)}\nDeadline Not given: {Global_var.deadline_Not_given}\nduplicate: {Global_var.duplicate}\ninserted: {Global_var.inserted}\nexpired: {Global_var.expired}\nQC Tenders: {Global_var.QC_Tenders}','tenderwizard.my', wx.OK | wx.ICON_INFORMATION)
+    browser.close()
+    sys.exit()
+chormedriver()
---
+++ Scraping_things.py
@@ -0,0 +1,156 @@
+import time
+from datetime import datetime
+import Global_var
+from Insert_On_Datbase import insert_in_Local
+import sys , os
+import string
+import time
+from datetime import datetime
+import html
+import re
+import wx
+app = wx.App()
+
+def remove_html_tag(string):
+    cleanr = re.compile('<.*?>')
+    main_string = re.sub(cleanr, '', string)
+    return main_string
+
+def scrap_data(browser,get_htmlsource,tender_link):
+
+    SegField = []
+    for data in range(42):
+        SegField.append('')
+    
+    a = True
+    while a == True:
+        try:
+            Contact_name = ''
+            Region = ''
+            for Contact_name in browser.find_elements_by_xpath('//*[@id="contactPersonspan"]'):
+                Contact_name = Contact_name.get_attribute('innerText')
+                Contact_name = string.capwords(str(Contact_name))
+                break
+            for Region in browser.find_elements_by_xpath('//*[@id="div_region"]'):
+                Region = Region.get_attribute('innerText').strip()
+                Region = string.capwords(str(Region))
+                break
+
+            SegField[2] = f'{Contact_name}<br>\n{Region} , Malaysia'
+            
+            Purchaser = get_htmlsource.partition('Buyer:')[2].partition("</td>")[0].strip()
+            if Purchaser != '':
+                Purchaser = remove_html_tag(Purchaser)
+                SegField[12] = Purchaser.strip().upper()
+
+            for Tender_id in browser.find_elements_by_xpath('//*[@id="tenderNumberspan"]'):
+                Tender_id = Tender_id.get_attribute('innerText')
+                SegField[13] = Tender_id.strip()
+                break
+
+            for Title in browser.find_elements_by_xpath('//*[@id="descOfWorkspan"]'):
+                Title = Title.get_attribute('innerText')
+                Title = string.capwords(str(Title))
+                SegField[19] = Title.strip()
+                break
+
+            for Tender_deadline in browser.find_elements_by_xpath('//*[@id="receiptOfTendToDatespan"]'):
+                Tender_deadline = Tender_deadline.get_attribute('innerText').strip()
+                datetime_object = datetime.strptime(Tender_deadline, '%d-%m-%Y %H:%M')
+                Tender_Deadline = datetime_object.strftime("%Y-%m-%d")
+                SegField[24] = Tender_Deadline
+                break
+
+            SegField[28] =  tender_link
+
+            vendor_Category = ''
+            tender_Remark = ''
+            tender_Type = ''
+            Multiple_Submission = ''
+            currency = ''
+            tenderStage = ''
+            for vendor_Category in browser.find_elements_by_xpath('//*[@id="vendorCategoryspan"]'):
+                vendor_Category = vendor_Category.get_attribute('innerText').strip()
+                vendor_Category = string.capwords(str(vendor_Category))
+                break
+            for tender_Remark in browser.find_elements_by_xpath('//*[@id="tendRemarkspan"]'):
+                tender_Remark = tender_Remark.get_attribute('innerText').strip()
+                tender_Remark = string.capwords(str(tender_Remark))
+                break
+            for tender_Type in browser.find_elements_by_xpath('//*[@id="tenderTypespan"]'):
+                tender_Type = tender_Type.get_attribute('innerText').strip()
+                tender_Type = string.capwords(str(tender_Type))
+                break
+            for Multiple_Submission in browser.find_elements_by_xpath('//*[@id="isMultipleSubmissionspan"]'):
+                Multiple_Submission = Multiple_Submission.get_attribute('innerText').strip()
+                Multiple_Submission = string.capwords(str(Multiple_Submission))
+                break
+            for currency in browser.find_elements_by_xpath('//*[@id="currencyspan"]'):
+                currency = currency.get_attribute('innerText').strip()
+                currency = string.capwords(str(currency))
+                break
+            for tenderStage in browser.find_elements_by_xpath('//*[@id="tenderStagespan"]'):
+                tenderStage = tenderStage.get_attribute('innerText').strip()
+                tenderStage = string.capwords(str(tenderStage))
+                break
+
+
+            SegField[18] = f'{SegField[19]}<br>\nVendor Category: {vendor_Category}<br>\nTender Remark: {tender_Remark}<br>\nTender Type: {tender_Type}<br>\nCurrency: {currency}<br>\ntender Stage: {tenderStage}'
+            
+            
+            SegField[14] = '2'
+            SegField[22] = "0"
+            SegField[26] = "0.0"
+            SegField[27] = "0"  # Financier
+            SegField[7] = 'MY'
+            SegField[31] = 'tenderwizard.my'
+            for SegIndex in range(len(SegField)):
+                print(SegIndex, end=' ')
+                print(SegField[SegIndex])
+                SegField[SegIndex] = html.unescape(str(SegField[SegIndex]))
+                SegField[SegIndex] = str(SegField[SegIndex]).replace("'", "''")
+
+            if len(SegField[19]) >= 200:
+                SegField[19] = str(SegField[19])[:200]+'...'
+
+            if len(SegField[18]) >= 1500:
+                SegField[18] = str(SegField[18])[:1500]+'...'
+
+            if SegField[19] == '':
+                wx.MessageBox(' Short Desc Blank ','tenderwizard.my', wx.OK | wx.ICON_INFORMATION)
+            else:
+                check_date(get_htmlsource, SegField)
+                
+
+            a = False
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print("Error ON : ", sys._getframe().f_code.co_name + "--> " + str(e), "\n", exc_type, "\n", fname, "\n",
+                  exc_tb.tb_lineno)
+            a = True
+            time.sleep(5)
+
+
+def check_date(get_htmlSource, SegField):
+    deadline = str(SegField[24])
+    curdate = datetime.now()
+    curdate_str = curdate.strftime("%Y-%m-%d")
+    try:
+        if deadline != '':
+            datetime_object_deadline = datetime.strptime(deadline, '%Y-%m-%d')
+            datetime_object_curdate = datetime.strptime(curdate_str, '%Y-%m-%d')
+            timedelta_obj = datetime_object_deadline - datetime_object_curdate
+            day = timedelta_obj.days
+            if day > 0:
+                insert_in_Local(get_htmlSource, SegField)
+            else:
+                print("Expired Tender")
+                Global_var.expired += 1
+        else:
+            print("Deadline Not Given")
+            Global_var.deadline_Not_given += 1
+    except Exception as e:
+        exc_type , exc_obj , exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("Error ON : " , sys._getframe().f_code.co_name + "--> " + str(e) , "\n" , exc_type , "\n" , fname , "\n" ,exc_tb.tb_lineno)
